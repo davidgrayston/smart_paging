26bf6e9f571f96f7669d5e0f919e9ae58617481b
Roland Michael dela Pena <roland@kapitbisig.com>
2011-06-24 22:09:20 +0800

    Issue #1196084 by dalin: Fixed WYSIWYG button causes Invalid XHTML: missing trailing slashes, absolute urls and uppercase tags, hanging tags, etc.

--- plugins/wysiwyg/smart_paging/smart_paging.js
+++ plugins/wysiwyg/smart_paging/smart_paging.js
@@ -28,12 +28,29 @@ Drupal.wysiwyg.plugins['smart_paging'] = {
    * Replace all <!--pagebreak--> tags with images.
    */
   attach: function(content, settings, instanceId) {
+    // @see http://drupal.org/node/510552#comment-3879096
+    // @todo Update this technique if that thread produces a better way to do
+    // image replacement.
+
     var smartPagingPagebreak = settings.smartPagingPagebreak;
+    var placeholder = this._getPlaceholder(settings);
+
     // Some WYSIWYGs (CKEditor) will strip the slash from single tags:
     // <foo /> becomes <foo>
-    var pagebreak = settings.smartPagingPagebreak.replace(/\/>/, '/?>').replace(/ /g, ' ?');
-    var pattern = new RegExp(pagebreak, 'g');
-    content = content.replace(pattern, this._getPlaceholder(settings));
+    var pagebreak = smartPagingPagebreak.replace(/\/>/, '/?>').replace(/ /g, ' ?');
+
+    // Remove unnecessary paragraph.
+    var pattern = new RegExp('<p>' + pagebreak + '</p>', 'ig');
+    content = content.replace(pattern, placeholder);
+    // Move breaks starting at the beginning of paragraphs to before them.
+    pattern = new RegExp('<p>' + pagebreak + '(<[^p])', 'ig');
+    content = content.replace(pattern, placeholder + '<p>$1');
+    // Move breaks starting at the end of to after the paragraphs.
+    pattern = new RegExp('([^p]>)' + pagebreak + '<\/p>', 'ig');
+    content = content.replace(pattern, '$1</p>' + placeholder);
+    // Other breaks.
+    content = content.replace(pagebreak, placeholder);
+
     return content;
   },
 
@@ -41,22 +58,48 @@ Drupal.wysiwyg.plugins['smart_paging'] = {
    * Replace images with <!--pagebreak--> tags in content upon detaching editor.
    */
   detach: function(content, settings, instanceId) {
-    var $content = $('<div>' + content + '</div>'); // No .outerHTML() in jQuery :(
-    // #404532: document.createComment() required or IE will strip the comment.
-    // #474908: IE 8 breaks when using jQuery methods to replace the elements.
-    $.each($('img.smart-paging', $content), function (i, elem) {
-      var pagebreak = $(settings.smartPagingPagebreak);
-      elem.parentNode.insertBefore(pagebreak[0], elem);
-      elem.parentNode.removeChild(elem);
-    });
-    return $content.html();
+    // @see http://drupal.org/node/510552#comment-3879096
+    // @todo Update this technique if that thread produces a better way to do
+    // image replacement.
+
+    var smartPagingPagebreak = settings.smartPagingPagebreak;
+
+    // Some WYSIWYGs (CKEditor) will strip the slash from single tags:
+    // <foo /> becomes <foo>
+    var pagebreak = smartPagingPagebreak.replace(/\/>/, '/?>').replace(/ /g, ' ?');
+
+    //   console.log('original:\n'+content);
+    // Replace (duplicate) placeholders within p tags with a single break.
+    var newContent = content.replace(/\s*<p[^>]*?>(?:\s*<img(?:\s*\w+=['"][^'"]*?['"]\s*)*?\s*class=['"][^'"]*?smart-paging[^'"]*?['"]\s*(?:\s*\w+=['"][^'"]*?['"]\s*)*?(?:\/)?>\s*)+<\/p>\s*/ig, smartPagingPagebreak);
+    //    console.log('1\n'+newContent);
+    // Replace all other placeholders.
+    newContent = newContent.replace(/<img(?:\s*\w+=['"][^'"]*?['"]\s*)*?\s*class=['"][^'"]*?smart-paging[^'"]*?['"]\s*(?:\s*\w+=['"][^'"]*?['"]\s*)*?(?:\/)?>/ig, smartPagingPagebreak);
+    //    console.log('2\n'+newContent);
+    // Fix paragraphs opening just before breaks.
+    var pattern = new RegExp('(?:' + pagebreak + ')*(<p[^>]*?>\s*)' + pagebreak, 'ig');
+    newContent = newContent.replace(pattern, smartPagingPagebreak + '$1');
+    //    console.log('3\n'+newContent);
+    // Remove duplicate breaks and any preceding whitespaces.
+    pattern = new RegExp('(?:\s*' + pagebreak + '){2,}' + pagebreak, 'ig');
+    newContent = newContent.replace(pattern, smartPagingPagebreak);
+    //    console.log('4\n'+newContent);
+    // Fix paragraphs ending after breaks.
+    pattern = new RegExp(pagebreak + '(\s*<\/p>)(?:' + pagebreak + ')*', 'ig');
+    newContent = newContent.replace(pattern, '$1' + smartPagingPagebreak);
+    //    console.log('5\n'+newContent);
+    // Remove duplicate breaks with trailing whitespaces.
+    pattern = new RegExp('(?:' + pagebreak + '\s*){2,}', 'ig');
+    newContent = newContent.replace(pattern, smartPagingPagebreak);
+    //    console.log('done\n'+newContent);
+    return newContent;
+
   },
 
   /**
    * Helper function to return a HTML placeholder.
    */
   _getPlaceholder: function (settings) {
-    return '<img src="' + settings.path + '/images/spacer.gif" alt="&lt;--pagebreak-&gt;" title="&lt;--pagebreak--&gt;" class="smart-paging drupal-content" />';
+    return '<img src="' + settings.path + '/images/spacer.gif" alt="&lt;--pagebreak--&gt;" title="&lt;--pagebreak--&gt;" class="smart-paging drupal-content" />';
   }
 };
 
